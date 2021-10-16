f585f58fbff3f78f2cf03d43a59345bc28d63f6d
Ferran Figueredo <ferranfg@gmail.com>
2021-10-16 10:40:46 +0200

    Escaping echo at widget.php

--- views/widget.php
+++ views/widget.php
@@ -7,41 +7,41 @@
 <section itemscope="" itemtype="http://schema.org/Book">
 	<?php if ($cover_src): ?>
 		<?php if ($landing_url): ?>
-			<a href="<?php echo $landing_url; ?>" title="<?php echo $title; ?>">
-				<img class="book-cover" src="<?php echo $cover_src; ?>" width="95" height="152" alt="<?php echo $title; ?>" itemprop="image" />
+			<a href="<?php echo esc_url($landing_url); ?>" title="<?php echo esc_attr($title); ?>">
+				<img class="book-cover" src="<?php echo esc_url($cover_src); ?>" width="95" height="152" alt="<?php echo esc_attr($title); ?>" itemprop="image" />
 			</a>
 		<?php else: ?>
-			<img class="book-cover" src="<?php echo $cover_src; ?>" width="95" height="152" alt="<?php echo $title; ?>" itemprop="image" />
+			<img class="book-cover" src="<?php echo esc_url($cover_src); ?>" width="95" height="152" alt="<?php echo esc_attr($title); ?>" itemprop="image" />
 		<?php endif; ?>
 	<?php endif; ?>
 	<h5 itemprop="name">
 		<?php if ($landing_url): ?>
-			<a href="<?php echo $landing_url; ?>" title="<?php echo $title; ?>"><?php echo $title; ?></a>
+			<a href="<?php echo esc_url($landing_url); ?>" title="<?php echo esc_attr($title); ?>"><?php echo esc_html($title); ?></a>
 		<?php else: ?>
-			<?php echo $title; ?>
+			<?php echo esc_html($title); ?>
 		<?php endif; ?>
 	</h5>
 	<?php if ($description): ?>
-		<p itemprop="description"><?php echo $description; ?></p>
+		<p itemprop="description"><?php echo esc_html($description); ?></p>
 	<?php endif; ?>
 	<?php if ($authors): ?>
-		<p itemprop="author" itemscope itemtype="http://schema.org/Person"><?php _e("By", "publisher"); ?> <span itemprop="name"><?php echo $authors; ?></span></p>
+		<p itemprop="author" itemscope itemtype="http://schema.org/Person"><?php _e("By", "publisher"); ?> <span itemprop="name"><?php echo esc_html($authors); ?></span></p>
 	<?php endif; ?>
 	<?php if (isset($instance['download']) and $instance['download']): ?>
 		<form method="post" action="">
-			<?php echo $wp_nonce_field; ?>
+			<?php echo esc_html($wp_nonce_field); ?>
 			<button type="submit" name="download_ebook" class="btn btn-default"><?php _e("Get your eBook!", "publisher"); ?></button>
 		</form>
 	<?php endif; ?>
 	<?php if (isset($instance['external']) and $instance['external'] and ($ibooks_url || $amazon_url)): ?>
 		<p class="book-links">
 			<?php if ($amazon_url): ?>
-				<a href="<?php echo $amazon_url; ?>">
+				<a href="<?php echo esc_url($amazon_url); ?>">
 					<img src="<?php echo MPL_BASEURL; ?>assets/imgs/amazon-apps-store-us-black.png" />
 				</a>
 			<?php endif; ?>
 			<?php if ($ibooks_url): ?>
-				<a href="<?php echo $ibooks_url; ?>">
+				<a href="<?php echo esc_url($ibooks_url); ?>">
 					<img src="<?php echo MPL_BASEURL; ?>assets/imgs/iBooks_icon_large.png" />
 				</a>
 			<?php endif; ?>
